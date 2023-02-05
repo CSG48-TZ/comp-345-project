46cf50c4f29d41800353e7b273e490b29d62c2e4
DaxInvader <DaxInvader@gmail.com>
2023-02-05 14:19:53 -0500

    Comments, Copy() and general optimisation of all 6 subclasses

--- Orders/Orders.cpp
+++ Orders/Orders.cpp
@@ -1,5 +1,8 @@
 #include "Orders.h"
 
+/*
+* Constructor
+*/
 Orders::Orders() {
 	this->target = 0;
 	this->from = 0;
@@ -9,33 +12,73 @@ Orders::Orders() {
 	this->orderNumber = 0;
 }
 
+/*
+* Gets the current order type as a string.
+*/
 string Orders::getCurrentOrder(void) {
 	return currentOrder;
 }
 
+/*
+* Gets the current player ID the order is targeted to.
+*/
 int Orders::getOrderTargetPlayer(void) {
 	return target;
 }
+
+/*
+* Gets the current army count affected with the order
+*/
 int Orders::getOrderArmyCount(void) {
 	return armyCount;
 }
+
+/*
+* Gets the current player ID the order is assigned to.
+*/
 int Orders::getOrderIssuer(void) {
 	return from;
 }
 
+/*
+* Gets the order ID.
+*/
 int Orders::getOrderNumber() {
 	return orderNumber;
 }
 
+/*
+* Gets the target the location ID
+*/
 int Orders::getTargetLocation() {
 	return targetLocation;
 }
 
+/*
+* Gets the location ID the order is coming from
+*/
 int Orders::getIssuerLocation() {
 	return fromLocation;
 }
 
+/*
+* Validate function, will be overriden by the subclasses
+*/
+bool Orders::validate() {
+	return false;
+}
 
+/*
+* execute function, will be overriden by the subclasses
+*/
+bool Orders::execute() {
+	return false;
+}
+
+
+/*
+* toString() Implementation.
+*/
 string Orders::toString(void) {
 	string returnString;
 	returnString.append("\nOrder#: ");
@@ -55,7 +98,15 @@ string Orders::toString(void) {
 	return returnString;
 }
 
+/****************ORDERS LIST CLASS****************/
 
+/*
+* This class creates a doubly linked list to store all the ORders currently requested by a player.
+*/
+
+/*
+* Constructor
+*/
 OrdersList::OrdersList(int player) {
 	this->player = player;
 	currentNumberOfOrders = 0;
@@ -64,12 +115,21 @@ OrdersList::OrdersList(int player) {
 	orderNumber = 0;
 }
 
+/*
+* Adds an Order to the list.
+*/
 void OrdersList::add(Orders order) {
 	ordersList.push_front(order);
 	//Update lastOrderModifiedIndex
 	lastOrderModifiedIndex = ordersList.size() - 1;
 }
 
+/*
+* Moves the order in the list. Takes an index of the order to move, and a target index.
+* 
+* Moves order from index "index" to index "toIndex"
+* 
+*/
 void OrdersList::move(int index, int toIndex) {
 	//Creates Iterator
 	list<Orders>::iterator it;
@@ -93,6 +153,9 @@ void OrdersList::move(int index, int toIndex) {
 	lastOrderModifiedIndex = toIndex;
 }
 
+/*
+* Removes the order at the requested Index
+*/
 void OrdersList::remove(int index) {
 	//Creates Iterator
 	list<Orders>::iterator it;
@@ -108,44 +171,87 @@ void OrdersList::remove(int index) {
 	//Update lastOrderModifiedIndex
 	lastOrderModifiedIndex = index;
 
-
-	//TODO send error if the order doesn't match.
 }
 
+/*
+* Validates the whole orders lists.
+*/
 bool OrdersList::validate()
 {
-	return false;
+	list<Orders>::iterator it;
+
+	for (it = ordersList.begin(); it != ordersList.end(); it++) {
+		Orders current = *it;
+		if (!current.validate()) {
+			return false;
+		}
+	}
+
+	return true;
 }
 
+/*
+* Executes all the orders.
+*/
+bool OrdersList::execute()
+{
+	list<Orders>::iterator it;
+
+	for (it = ordersList.begin(); it != ordersList.end(); it++) {
+		Orders current = *it;
+		if (!current.execute()) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+/*
+* Gets the current size of the list, i.e. the number of orders the player has.
+*/
 int OrdersList::getCurrentNumberOfOrders()
 {
 	return ordersList.size();
 }
 
+/*
+* Helper function, returns the index of the last modified order (addition, deletion, etc..)
+*/
 int OrdersList::getLastOrderModified()
 {
 	return lastOrderModifiedIndex;
 }
 
+/*
+* Gets the last Order in the list.
+*/
 Orders OrdersList::getLastOrder()
 {
 	return ordersList.back();
 }
 
+/*
+* Gets the first Order in the list.
+*/
 Orders OrdersList::getNextOrder()
 {
 	return ordersList.front();
 }
 
+/*
+* Returns the list.
+*/
 list<Orders> OrdersList::getCurrentOrdersList()
 {
 	return ordersList;
 }
 
-bool OrdersList::execute(int index) {
-	return false;
-}
+/****************DEPLOY ORDER LIST CLASS****************/
 
+/*
+* Constructor
+*/
 Deploy::Deploy(int target, int from, int armyCount, int targetLocation, int fromLocation, int orderNumber) {
 	this->target = target;
 	this->from = from;
@@ -153,16 +259,38 @@ Deploy::Deploy(int target, int from, int armyCount, int targetLocation, int from
 	this->targetLocation = targetLocation;
 	this->fromLocation = fromLocation;
 	this->orderNumber = orderNumber;
+
+	currentOrder = "Deploy";
 }
 
+/*
+* Validates the order, returns true or false
+*/
 bool Deploy::validate() {
 	return false;
 }
 
+/*
+* Executes the Order returns true is successful
+*/
 bool Deploy::execute() {
 	return false;
 }
 
+
+/*
+* Copy function (REQUIREMENT). 
+*/
+Deploy Deploy::copy(Deploy order) {
+	Deploy copy = Deploy(this->target, this->from, this->armyCount, this->targetLocation, this->fromLocation, this->orderNumber);
+	return copy;
+}
+
+/****************ADVANCE ORDER LIST CLASS****************/
+
+/*
+* Constructor
+*/
 Advance::Advance(int target, int from, int armyCount, int targetLocation, int fromLocation, int orderNumber) {
 	this->target = target;
 	this->from = from;
@@ -170,16 +298,37 @@ Advance::Advance(int target, int from, int armyCount, int targetLocation, int fr
 	this->targetLocation = targetLocation;
 	this->fromLocation = fromLocation;
 	this->orderNumber = orderNumber;
+
+	currentOrder = "Advance";
 }
 
+/*
+* Validates the order, returns true or false
+*/
 bool Advance::validate() {
 	return false;
 }
 
+/*
+* Executes the Order returns true is successful
+*/
 bool Advance::execute() {
 	return false;
 }
 
+/*
+* Copy function (REQUIREMENT).
+*/
+Advance Advance::copy(Advance order) {
+	Advance copy = Advance(this->target, this->from, this->armyCount, this->targetLocation, this->fromLocation, this->orderNumber);
+	return copy;
+}
+
+/****************BOMB ORDER LIST SUB CLASS****************/
+
+/*
+* Constructor
+*/
 Bomb::Bomb(int target, int from, int armyCount, int targetLocation, int fromLocation, int orderNumber) {
 	this->target = target;
 	this->from = from;
@@ -187,16 +336,37 @@ Bomb::Bomb(int target, int from, int armyCount, int targetLocation, int fromLoca
 	this->targetLocation = targetLocation;
 	this->fromLocation = fromLocation;
 	this->orderNumber = orderNumber;
+
+	currentOrder = "Bomb";
 }
 
+/*
+* Validates the order, returns true or false
+*/
 bool Bomb::validate() {
 	return false;
 }
 
+/*
+* Executes the Order returns true is successful
+*/
 bool Bomb::execute() {
 	return false;
 }
 
+/*
+* Copy function (REQUIREMENT).
+*/
+Bomb Bomb::copy(Bomb order) {
+	Bomb copy = Bomb(this->target, this->from, this->armyCount, this->targetLocation, this->fromLocation, this->orderNumber);
+	return copy;
+}
+
+/****************BLOCKADE ORDER LIST SUB CLASS****************/
+
+/*
+* Constructor
+*/
 Blockade::Blockade(int target, int from, int armyCount, int targetLocation, int fromLocation, int orderNumber) {
 	this->target = target;
 	this->from = from;
@@ -204,16 +374,37 @@ Blockade::Blockade(int target, int from, int armyCount, int targetLocation, int 
 	this->targetLocation = targetLocation;
 	this->fromLocation = fromLocation;
 	this->orderNumber = orderNumber;
+
+	currentOrder = "Blockade";
 }
 
+/*
+* Validates the order, returns true or false
+*/
 bool Blockade::validate() {
 	return false;
 }
 
+/*
+* Executes the Order returns true is successful
+*/
 bool Blockade::execute() {
 	return false;
 }
 
+/*
+* Copy function (REQUIREMENT).
+*/
+Blockade Blockade::copy(Blockade order) {
+	Blockade copy = Blockade(this->target, this->from, this->armyCount, this->targetLocation, this->fromLocation, this->orderNumber);
+	return copy;
+}
+
+/****************AIRLIFT ORDER LIST SUB CLASS****************/
+
+/*
+* Constructor
+*/
 Airlift::Airlift(int target, int from, int armyCount, int targetLocation, int fromLocation, int orderNumber) {
 	this->target = target;
 	this->from = from;
@@ -221,16 +412,38 @@ Airlift::Airlift(int target, int from, int armyCount, int targetLocation, int fr
 	this->targetLocation = targetLocation;
 	this->fromLocation = fromLocation;
 	this->orderNumber = orderNumber;
+
+	currentOrder = "Airlift";
+
 }
 
+/*
+* Validates the order, returns true or false
+*/
 bool Airlift::validate() {
 	return false;
 }
 
+/*
+* Executes the Order returns true is successful
+*/
 bool Airlift::execute() {
 	return false;
 }
 
+/*
+* Copy function (REQUIREMENT).
+*/
+Airlift Airlift::copy(Airlift order) {
+	Airlift copy = Airlift(this->target, this->from, this->armyCount, this->targetLocation, this->fromLocation, this->orderNumber);
+	return copy;
+}
+
+/****************NEGOCIATE ORDER LIST SUB CLASS****************/
+
+/*
+* Constructor
+*/
 Negociate::Negociate(int target, int from, int armyCount, int targetLocation, int fromLocation, int orderNumber) {
 	this->target = target;
 	this->from = from;
@@ -238,12 +451,28 @@ Negociate::Negociate(int target, int from, int armyCount, int targetLocation, in
 	this->targetLocation = targetLocation;
 	this->fromLocation = fromLocation;
 	this->orderNumber = orderNumber;
+
+	currentOrder = "Negociate";
 }
 
+/*
+* Validates the Negociate order, returns true or false
+*/
 bool Negociate::validate() {
 	return false;
 }
 
+/*
+* Executes the Order returns true is successful
+*/
 bool Negociate::execute() {
 	return false;
+}
+
+/*
+* Copy function (REQUIREMENT).
+*/
+Negociate Negociate::copy(Negociate order) {
+	Negociate copy = Negociate(this->target, this->from, this->armyCount, this->targetLocation, this->fromLocation, this->orderNumber);
+	return copy;
 }
--- Orders/Orders.h
+++ Orders/Orders.h
@@ -17,6 +17,8 @@ public:
 	int getIssuerLocation(void);
 	int getOrderNumber(void);
 	string toString(void);
+	bool validate();
+	bool execute();
 
 protected:
 	string currentOrder;
@@ -36,7 +38,7 @@ public:
 	void move(int index, int toIndex);
 	void remove(int index);
 	bool validate();
-	bool execute(int index);
+	bool execute();
 	int getCurrentNumberOfOrders();
 	int getLastOrderModified();
 	Orders getLastOrder();
@@ -61,34 +63,40 @@ public:
 	Deploy(int target, int from, int armyCount, int targetLocation, int fromLocation, int orderNumber);
 	bool validate(void);
 	bool execute();
+	Deploy copy(Deploy order);
 };
 
 class Advance : public Orders {
 	Advance(int target, int from, int armyCount, int targetLocation, int fromLocation, int orderNumber);
 	bool validate(void);
 	bool execute();
+	Advance copy(Advance order);
 };
 
 class Bomb : public Orders {
 	Bomb(int target, int from, int armyCount, int targetLocation, int fromLocation, int orderNumber);
 	bool validate(void);
 	bool execute();
+	Bomb copy(Bomb order);
 };
 
 class Blockade : public Orders {
 	Blockade(int target, int from, int armyCount, int targetLocation, int fromLocation, int orderNumber);
 	bool validate(void);
 	bool execute();
+	Blockade copy(Blockade order);
 };
 
 class Airlift : public Orders {
 	Airlift(int target, int from, int armyCount, int targetLocation, int fromLocation, int orderNumber);
 	bool validate(void);
 	bool execute();
+	Airlift copy(Airlift order);
 };
 
 class Negociate : public Orders {
 	Negociate(int target, int from, int armyCount, int targetLocation, int fromLocation, int orderNumber);
 	bool validate(void);
 	bool execute();
+	Negociate copy(Negociate order);
 };
